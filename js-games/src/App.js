ffaf08303a466b30cb4b63f4a5dab656b2d44268
Joel <jestokes@odu.edu>
2021-04-29 16:56:07 -0400

    Adding React Router Dom & Material UI

--- js-games/src/App.js
+++ js-games/src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";  
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      Coming Soon
-    </div>
+    <Router>
+      <div className="App">
+        Coming Soon
+        <Home/>
+      </div>
+    </Router>
   );
 }
 
---
+++ js-games/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { Typography } from '@material-ui/core';
+
+function Home() {
+    return (
+        <div>
+            <Typography>
+                Placeholder 
+            </Typography>
+        </div>
+    )
+}
+
+export default Home;
