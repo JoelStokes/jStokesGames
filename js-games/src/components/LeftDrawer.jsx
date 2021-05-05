7fbd7ec1d241debf4a586e1bc3e776269923eeae
Joel <jestokes@odu.edu>
2021-05-05 14:57:47 -0400

    Drawer Buttons & Icons

--- js-games/src/components/LeftDrawer.jsx
+++ js-games/src/components/LeftDrawer.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom";
-import { Typography, AppBar, Toolbar, IconButton, Button, Drawer, MenuItem } from '@material-ui/core';
+import { List, ListItem, ListItemIcon, ListItemText, Drawer, Divider } from '@material-ui/core';
 
 import {
     Switch,
     Route
 } from "react-router-dom";  
+import '../styles/Components.css';
 
 import HomeIcon from '@material-ui/icons/Home';
 import WorkIcon from '@material-ui/icons/Work';
@@ -13,7 +14,9 @@ import ContactIcon from '@material-ui/icons/PermContactCalendar';
 import TeamIcon from '@material-ui/icons/GroupWork';
 import SoloIcon from '@material-ui/icons/Gamepad';
 
-import SoloAlt from '@material-ui/icons/DeveloperMode';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import ItchIcon from '../images/itch-io-icon.png';
 
 export default class LeftDrawer extends React.Component {
     constructor(props) {
@@ -40,8 +43,54 @@ export default class LeftDrawer extends React.Component {
                   onBackdropClick={this.handleClose}
                   onRequestChange={(open) => this.setState({open})}
               >
-                  <Link to="/home"><MenuItem onTouchTap={this.handleClose}>Home</MenuItem></Link>
-                  <Link to="/topics"><MenuItem onTouchTap={this.handleClose}>404</MenuItem></Link>
+                <List component="nav">
+                    <ListItem button>
+                    <ListItemIcon>
+                        <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Home" />
+                    </ListItem>
+                    <ListItem button>
+                    <ListItemIcon>
+                        <SoloIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Solo Game Dev" />
+                    </ListItem>
+                    <ListItem button>
+                    <ListItemIcon>
+                        <TeamIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Team Game Dev" />
+                    </ListItem>
+                    <ListItem button>
+                    <ListItemIcon>
+                        <WorkIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Professional" />
+                    </ListItem>
+                    <ListItem button>
+                    <ListItemIcon>
+                        <ContactIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Contact" />
+                    </ListItem>
+                </List>
+                <List className="drawerBottom">
+                    <Divider />
+                    <ListItem button>
+                    <ListItemIcon>
+                        <LinkedInIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="LinkedIn" />
+                    </ListItem>
+                    <ListItem button>
+                    <ListItemIcon>
+                        <GitHubIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Github" />
+                    </ListItem>
+                    <img src={ItchIcon} className='outsideIcon' alt="ItchIO" />
+                </List>
               </Drawer>
           </div>
       );
