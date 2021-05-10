17bcf52cecfc9a37eee106ba1ab0190c41e80283
Joel <jestokes@odu.edu>
2021-05-10 17:54:11 -0400

    Switch Typewriter CSS->JS, wasn't Mobile Friendly

--- js-games/src/pages/Home.jsx
+++ js-games/src/pages/Home.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { Typography, Grid, Fade, Slide } from '@material-ui/core';
 import Footer from '../components/Footer'
 import '../styles/Home.css';
+import Typewriter from 'typewriter-effect/dist/core';
 
 function Home() {
     const [titleFade, setTitleFade] = useState(false);
-    const [subtitleFade, setSubtitleFade] = useState(false);
 
     const effects = {
         timers: {
@@ -25,7 +25,19 @@ function Home() {
     };   
     
     const subtitleTimerElapsed = () => {
-        setSubtitleFade(true);
+    }
+
+    const startType = () => {
+        var app = document.getElementById('app');
+        var typewriter = new Typewriter(app, {
+            loop: false,
+            delay: 50,
+            autoStart: false
+        });   
+        typewriter 
+        .pauseFor(350)
+        .typeString('Front-End Developer & Game Designer')
+        .start();
     }
 
     return (
@@ -38,20 +50,22 @@ function Home() {
                 justify="center"
                 style={{ minHeight: '100vh' }}  //Style not reading properly from Home.css?
                 >
-                <Grid item xs={12} onload={setTimeout(titleTimerElapsed, effects.timers.title)}>
+                <Grid item xs={12} onload={setTimeout(titleTimerElapsed, effects.timers.title), startType()}>
                     <Slide direction="up" in={titleFade} mountOnEnter unmountOnExit timeout={effects.durations.slide}>
                         <div>
                             <Fade in={titleFade} timeout={effects.durations.fade}>
-                                <Typography variant="h1" component="h3" className="Title">
+                                <Typography variant="h1" component="h3" className="Title" style={{textAlign: 'center'}}>
                                     Joel Stokes   
                                 </Typography>
                             </Fade>
                         </div>
                     </Slide>
                 </Grid>
-                <Grid item xs={12}  /*2nd Grid Slide causing 1st to readjust?*/>
-                    <Typography variant="h4" component="h5" gutterBottom className="Typewriter">
-                        Front-End Developer & Game Designer
+                <Grid item xs={12}>
+                    <Typography component="h4" variant="h6" gutterBottom>
+                        <div id="app" style={{
+                                  textAlign: 'center'
+                        }}/>
                     </Typography>
                 </Grid>
             </Grid>
